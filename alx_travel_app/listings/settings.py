edb6d517342a7be2a08227657d3bb464a50657b0
Sama Mostafa <samam6778@gmail.com>
2026-01-11 21:01:27 +0200

    Add settings for Django application

---
+++ alx_travel_app/listings/settings.py
@@ -0,0 +1,15 @@
+SECRET_KEY = 'dummy-secret-key'
+
+INSTALLED_APPS = []
+
+DEBUG = True
+
+INSTALLED_APPS = [
+    ...
+    'listings',
+]
+CELERY_BROKER_URL = 'amqp://localhost'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'noreply@alxtravel.com'
